cd44d1aedf385b520dc115506434ddfc822535ef
hamoro <40411863+hamoro@users.noreply.github.com>
2018-06-22 06:25:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,23 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+var Canvas = require('canvas');// npm i canvas
+var jimp = require('jimp');// npm i jimp 
+const fs = require("fs");// npm i fs
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+      bot.on('guildMemberAdd', member => {
+      const welcomer =  member.guild.channels.find('name', 'chat');//اسم الروم الي يرحب فيه
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
-
-
-bot.on("guildMemberAdd", member => {
-      const welcomer = bot.channels.get("451365319772995594");
-      //*let welcomer = member.guild.channels.find("name","chat");
-      if(!welcomer) return;
-      if(welcomer) {
-         moment.locale('ar-ly');
-         var h = member.user;
-        let heroo = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(h.avatarURL)
-        .setAuthor(h.username,h.avatarURL)
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
-         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:heroo});          
-         
       var Canvas = require('canvas')
       var jimp = require('jimp')
-      
+
       const w = ['./img/w1.png',
       './img/w2.png',
       './img/w3.png',
       './img/w4.png',
       './img/w5.png',
-      './img/w6.png',
       './img/w7.png',
-      './img/w8.png',
-      './img/w9.png',
-      './img/w10.png',
-      './img/w11.png',
-      './img/w12.png',];
-      
+      './img/w8.png'];
+
               let Image = Canvas.Image,
                   canvas = new Canvas(401, 202),
                   ctx = canvas.getContext('2d');
@@ -59,44 +33,44 @@ bot.on("guildMemberAdd", member => {
                   let ground = new Image;
                   ground.src = Background;
                   ctx.drawImage(ground, 0, 0, 401, 202);
-      
+
       })
-      
+
                       let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
                       jimp.read(url, (err, ava) => {
                           if (err) return console.log(err);
                           ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                               if (err) return console.log(err);
-      
-                              //AVATARً
+
+                              
                               let Avatar = Canvas.Image;
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.drawImage(ava, 152, 27, 95, 95);
-                              
+
                                                       //wl
                               ctx.font = '20px Arial Bold';
-                              ctx.fontSize = '20px';
+                              ctx.fontSize = '15px';
                               ctx.fillStyle = "#FFFFFF";
                               ctx.textAlign = "center";
                                                          ctx.fillText(member.user.username, 200, 154);
-                              
-                              //NAMEً
+
+                              //NAME
                               ctx.font = '20px Arial';
                               ctx.fontSize = '28px';
                               ctx.fillStyle = "#FFFFFF";
                               ctx.textAlign = "center";
-      ctx.fillText(`انت العضو رقم  ${member.guild.memberCount} !! `
+                                    ctx.fillText(`انت العضو رقم${member.guild.memberCount} `
                               , 200, 190);
-    welcomer.sendFile(canvas.to1Buffer())
-      
-      
-      
+
+ welcomer.sendFile(canvas.toBuffer())
+
+
+
       })
       })
-      
-      }
       });
+
 
 
 // THIS  MUST  BE  THIS  WAY
