1d4395fd98b87b01b2f2e65f906ceb6f508eade2
hamoro <40411863+hamoro@users.noreply.github.com>
2018-06-23 15:48:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,44 +11,75 @@ client.on('message', message => {
       }
 });
 
-const moment = require("moment");
-client.on('message', async message => {
-    if (!message.channel.guild) return undefined;
-    let time = moment().format('Do MMMM YYYY , hh:mm');
-    let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "bc")) {
-        if(!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.reply("**# You don't have the needed permissions!**");
-        if(!args) return message.reply("**# Supply a message!**");
-        message.channel.send(`\`\`- Name:\`\`\n${message.author}\n\n\`\`- Date:\`\`\n${time}\n\n\`\`- Message:\`\`\n${args}\n\n__# | You have 15s to say Yes or No__`)
-.then(() => {
-  message.channel.awaitMessages(response => response.content === 'yes', {
-    max: 1,
-    time: 15000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-                let bcEmbed = new Discord.RichEmbed()
-          .setAuthor(`${message.author.username}#${message.author.discriminator}`,message.author.avatarURL)
-          .setThumbnail(message.guild.iconURL || message.guild.avatarURL)
-          .addField('- Sender:',message.author)
-          .addField('- Server:',message.guild.name)
-          .addField('- Message:',`\`\`\`${args}\`\`\``);
-          message.guild.members.forEach(m => m.sendMessage(bcEmbed));
-          message.channel.send(`**Done!, Sent the message to: \`${message.guild.members.size}\` members!**`);
+const fs = require('fs');
+client.on("guildMemberAdd", member => {
       
-  });
-});
-    } else {
-          message.channel.awaitMessages(response => response.content === 'no', {
-    max: 1,
-    time: 15000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.channel.send("__- Canceled!__")
-    });
-    }
-});
+      const welcomer = member.guild.channels.find("name","welcome"); //اسم روم
+                
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+      
+      const mi = ['./img/w1.png','./img/w2.png']; //يمكن ضيف '/img/w3.png','/img/w4.png'...
+      
+     
+      
+              let Image = Canvas.Image,
+                  canvas = new Canvas(401, 202),
+                  ctx = canvas.getContext('2d');
+              ctx.patternQuality = 'bilinear';
+              ctx.filter = 'bilinear';
+              ctx.antialias = 'subpixel';
+              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+              ctx.shadowOffsetY = 2;
+              ctx.shadowBlur = 2;
+              fs.readFile(`${mi[Math.floor(Math.random() * mi.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 401, 202);
+      
+      })
+      
+                     let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+      
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.drawImage(ava, 152, 27, 95, 95);
+                            //member number..
+                        ctx.font = '13px Arial';
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#FFFFFF";
+                        ctx.textAlign = "center";
+                        ctx.fillText(`انت العضو رقم  ${member.guild.memberCount} ! `, 340 , 98);
+    
+                                                      //name
+                              ctx.font = '20px Arial Bold';
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+                                                         ctx.fillText(`${member.user.username}`, 200, 154);
+                             
+                             //server name
+                              ctx.font = '20px Arial';
+                              ctx.fontSize = '28px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+   ctx.fillText(` Welcome to ${member.guild.name} server` , 200, 190);
+   
+   
+
+    welcomer.sendFile(canvas.toBuffer())
+      
+      });
+      });
+      });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
