0c4e224ce703f86fc73f963bcfeff87c7b8dce53
hamoro <40411863+hamoro@users.noreply.github.com>
2018-06-24 22:01:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -391,5 +391,16 @@ client.on('message', message => {
   }
 });	
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`KD_4Ever`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
